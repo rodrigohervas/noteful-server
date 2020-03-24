e740a069bb8e119abe7c1b89f20492b46af8736e
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-03-23 20:30:30 -0400

    Modified files for deployment

--- src/knexContext.js
+++ src/knexContext.js
@@ -3,7 +3,7 @@ const config = require('./config')
 
 const db = knex({
     client: 'pg', 
-    connection: (config.NODE_ENV === 'test') ? config.TEST_DB_URL : config.DB_URL
+    connection: (config.NODE_ENV === 'test') ? config.TEST_DATABASE_URL : config.DATABASE_URL
 })
 
 module.exports = db
